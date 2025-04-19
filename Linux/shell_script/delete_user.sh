f48cc4739210a3f8a57aa41583c2db610c542269
codeisgod <digambarbangdejob@gmail.com>
2025-04-19 22:45:53 +0530

    updated delete user

--- Linux/shell_script/delete_user.sh
+++ Linux/shell_script/delete_user.sh
@@ -5,6 +5,10 @@ echo "========== Deletion of User Started ==========="
 sudo userdel -r "$1"    #delete user with directory
 #sudo userdel "$1"     #delete user only without directory
 
+cat /etc/passwd | grep "$1" | wc    # /etc/passwd is listed all users
+
+echo "as wordcount is 0, user deleted successfully."
+
 echo "========== Deletion of User Completed ==========="
 
 <<run
