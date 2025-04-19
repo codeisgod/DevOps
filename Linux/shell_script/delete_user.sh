c7a76c4bc449b2eac75cfd741b198eed3b506b45
codeisgod <digambarbangdejob@gmail.com>
2025-04-19 22:57:34 +0530

    updated delete user

--- Linux/shell_script/delete_user.sh
+++ Linux/shell_script/delete_user.sh
@@ -6,6 +6,10 @@ sudo userdel -r "$1"    #delete user with directory
 #sudo userdel "$1"     #delete user only without directory
 
 cat /etc/passwd | grep "$1" | wc    # /etc/passwd is listed all users
+# /etc/passwd is listed all users -> grep (filter only username)
+#checking wc -> word count to make sure it have 0 values
+
+#cat /etc/passwd | grep "$1" | wc | awk '{print $1}'
 
 echo "as wordcount is 0, user deleted successfully."
 
